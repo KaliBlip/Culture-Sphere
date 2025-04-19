f55ed958c2a1757b4a54b89b2172827d9f03de45
KaliBlip <69836195+KaliBlip@users.noreply.github.com>
2025-04-19 19:57:51 +0000

    Add base URL constant for deployment configuration in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 
+const base = 'https://kaliblip.github.io/Culture-sphere/'
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
